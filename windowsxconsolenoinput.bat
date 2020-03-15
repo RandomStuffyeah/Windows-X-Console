8e5d97f2df514e4e086f9a6b10c72ae2ad32671a
zdrmonster1 <37600325+guest123guest@users.noreply.github.com>
2020-03-14 23:47:50 -0400

    Create windowsxconsolenoinput.bat

---
+++ windowsxconsolenoinput.bat
@@ -0,0 +1,19 @@
+@title Microsoft Virtual Console
+@echo off
+:command
+echo Windows X-Console Beta 2003
+echo (C) Microsoft 1985-2021
+title Windows X-Console
+:bsod
+cls
+color 1F
+rem dat makz it blue har har har...
+title Windows X-Console [crashed]
+:bsod2
+echo WINDOWS
+echo ERROR OCCEREED
+:iedos
+echo connecting to GOOGLE.COM
+echo DONE
+echo GOOGLE
+echo Start Searching
