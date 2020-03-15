f10d604f525a33d2bb1e8446877667fdc5df185c
zdrmonster1 <37600325+guest123guest@users.noreply.github.com>
2020-03-14 23:24:58 -0400

    Update Windows X Console.bat

--- Windows X Console.bat
+++ Windows X Console.bat
@@ -1,24 +1,21 @@
 @title Windows X-Console Beta 1995 - Microsoft Virtual Console
 @echo off
-color 1F
-rem dat makz it blue har har har...
-goto dongle
 pause
 :command
-echo Windows X-Console Beta 1995
+echo Windows X-Console Beta 1996
 echo (C) Microsoft 2020
 goto console
-:dongle
-echo Please install dongle on COM 1
-pause
-goto command
 :console
 echo | set /p=C:\
 Set /p command=
 if %command% == iexplore goto iedos
 if %command% == bsod goto bsod
 :bsod
+color 1F
+rem dat makz it blue har har har...
 cls
+color 1F
+rem dat makz it blue har har har...
 :bsod2
 echo WINDOWS
 echo ERROR OCCEREED
