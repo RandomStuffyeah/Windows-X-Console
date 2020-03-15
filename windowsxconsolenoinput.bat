84866c2c708242c66bac59e78038912cc4c138c5
zdrmonster1 <37600325+guest123guest@users.noreply.github.com>
2020-03-14 23:53:11 -0400

    Update windowsxconsolenoinput.bat

--- windowsxconsolenoinput.bat
+++ windowsxconsolenoinput.bat
@@ -1,20 +1,12 @@
-@title Microsoft Virtual Console
 @echo off
 :command
-echo Windows X-Console Beta 2003
+echo Windows X-Console
 echo (C) Microsoft 1985-2021
-title Windows X-Console
-:bsod
-cls
-color 1F
-rem dat makz it blue har har har...
-title Windows X-Console [crashed]
 :bsod2
 echo WINDOWS
 echo ERROR OCCEREED
 :iedos
 echo connecting to GOOGLE.COM
-ping google.com
 echo DONE
 echo GOOGLE
 echo Start Searching
